ea8419ff37db7d784bd5cabbca77c3d0f2123ce5
Rfahren <Lotblue5@aol.com>
2017-12-05 12:41:42 -0700

    Changed for loops and while loop. moved ea into for loop

--- OneDrive/School/Fall 2017/MECH 105/Homework/Homework 22/heun_Fahrenkrug.m
+++ OneDrive/School/Fall 2017/MECH 105/Homework/Homework 22/heun_Fahrenkrug.m
@@ -25,26 +25,30 @@ if nargin < 6 || isempty(maxit)
 end
 % initalized variables/ preallocation of y
 iter=0;
-ea=100;
 a=tspan(1);
 b=tspan(2);
-m=(b-a)/h;
+m=(b-a)/h; % number of iterations
 t=a:h:b;
 y=zeros(1,m+1);
 y(1)=y0;
 % loops for Huen's method
-while iter<maxit && es<ea
-    iter=iter+1;
-    for i=1:m
-        k=dpdt(t(i),y(i));
-        k2=dpdt(t(i+1),y(i+1));
+for i=1:m
+        ea=100;
+        k=feval(dydt,t(i),y(i));
+        y(i+1)=y(i)+k*h;
+    while iter<maxit && es<=ea
+        iter=iter+1;
+        yold=y(i+1);
+        k2=feval(dydt,t(i+1),y(i+1));
         y(i+1)=y(i)+(h/2)*(k+k2);
+            if y(i+1)~=0
+               ea=abs((y(i+1)-yold)/y(i+1))*100;
+            end
     end
-    ea=abs((y(i)-y(i-1))/y(i))*100;
 end
 % plots
 plot(t,y);
-title('Proof of rise in failed condoms')
+%title('Proof of rise in failed condoms')
 xlabel('Time')
 ylabel('Population')
 end
